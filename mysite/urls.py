9cb18065d453f0e632cf1a85e3fe9237aa74d5ae
Nitindholera <nitin716sharma@gmail.com>
2021-10-18 18:26:07 +0530

    Removed broken links bug in polls app

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
+    path('Django-Voting-Site/admin/', admin.site.urls),
+    path('Django-Voting-Site/polls/', include('polls.urls')),
 ]
